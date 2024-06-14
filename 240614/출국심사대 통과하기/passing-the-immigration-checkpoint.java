f83509cb73b74236aadcc51548a8e46481cf2dc9
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2024-06-14 15:03:29 +0900

    Title: 출국심사대 통과하기, Time: 99ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240614/출국심사대 통과하기/passing-the-immigration-checkpoint.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	static PriorityQueue<Person> pq = new PriorityQueue<>();
+
+	static int answer;
+
+	public static void main(String[] args) throws IOException {
+		input();
+		solve();
+		System.out.println(answer);
+	}
+
+	static void input() throws IOException {
+		N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int time = Integer.parseInt(st.nextToken());
+			pq.add(new Person(start, time));
+		}
+	}
+
+	static void solve() {
+		int sleep = 1;
+		for (int time = 1; time <= 1_000_000 && !pq.isEmpty(); time++) {
+			if (sleep > time) {
+				continue;
+			}
+			if (pq.peek().start <= time) {
+				sleep = time + pq.poll().time;
+			}
+		}
+		answer = sleep;
+	}
+
+	static class Person implements Comparable<Person> {
+		int start;
+		int time;
+
+		public Person(int start, int end) {
+			this.start = start;
+			this.time = end;
+		}
+
+		@Override
+		public int compareTo(Person o) {
+			return Integer.compare(start, o.start);
+		}
+	}
+}
