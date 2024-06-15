6803029d9b2bd3f5fbd624fe1675f554c1ae671f
Sei Kim <74192619+KIMSEI1124@users.noreply.github.com>
2024-06-15 15:53:38 +0900

    Title: 섞기 전 카드 위치, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240615/섞기 전 카드 위치/card-position-before-shuffling.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+
+    static Map<Integer, Integer> map = new HashMap<>();
+    static int[] cards;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        cards = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            map.put(Integer.parseInt(st.nextToken()), i);
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= 1; i++) {
+            int[] temp = cards.clone();
+            for (int j = 1; j <= N; j++) {
+                temp[j] = cards[map.get(j)];
+            }
+            cards = temp;
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            answer.append(cards[i]).append("\n");
+        }
+        System.out.println(answer);
+    }
+}
